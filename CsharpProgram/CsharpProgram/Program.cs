1657e862d32d5efdda7731aabada9130867f0e50
dhill22 <dhill22@ycp.edu>
2019-03-04 17:50:12 -0500

    fixed inheritance issue

--- CsharpProgram/CsharpProgram/Program.cs
+++ CsharpProgram/CsharpProgram/Program.cs
@@ -13,13 +13,13 @@ namespace CsharpProgram
     //WITHIN SAME NAMESPACE CREATE ANOTHER CLASS:
     class SimpleClass
     {
-        //HANDLE MEMBERS:
+        //MEMBERS:
         private String word = " ";//INIT TO SPACE
 
-        //HANDLE METHODS:
-        public void Display()
+        //METHODS:
+        public void Display(String x)
         {
-            Console.WriteLine(word);
+            Console.WriteLine(x);
         }
         public String getWord()
         {
@@ -30,34 +30,54 @@ namespace CsharpProgram
             this.word = x;
         }
     }
+    class SimpleClass2 : SimpleClass//INHERITANCE SYNTAX
+    {
+        //MEMBERS:
+        //NOTE THIS CLASS ALSO HAS ACCESS TO DISPLAY()
+        protected String word2 = "Hello from the derived class";
+
+        //METHODS:
+        //NOTE THIS CLASS ALSO HAS ACCESS TO DISPLAY() FROM BASE CLASS
+        public String getWord2()
+        {
+            return this.word2;
+        }
+        public void setWord2(String x)
+        {
+            this.word2 = x;
+        }
+
+    }
     //MAIN CLASS PREPOPULATED:
     class Program
     {
         static void Main(string[] args)
-        {
-            //CREATED INSTANCE: 
+        {        
+            //LOCAL VARIABLES:
+            int userNum = 0;
+
+            //INSTANCES: 
             SimpleClass handle = new SimpleClass();
+            SimpleClass2 handle1 = new SimpleClass2();   
 
-            //DECLARATION OF VARIABLE:
-            int getNum = 0;
-
-            //WRITE TO CONSOLE:
+            //WRITE TO CONSOLE(W/CARRIAGE RETURN):
             Console.WriteLine("Hello World!");
 
+            //WRITE TO CONSOLE(W/O CARRIAGE RETURN):
+            Console.Write("Enter an interger: ");
+
             //USE OF CONVERT METHOD FOR CASTING:
-            Console.WriteLine("Enter an interger: ");
-            getNum = Convert.ToInt32(Console.ReadLine());//READLINE RETURNS A STRING
-            Console.WriteLine("You entered: " + getNum);//CONCATENATION LIKE JAVA
+            userNum = Convert.ToInt32(Console.ReadLine());//READLINE RETURNS A STRING
+            Console.WriteLine("You entered: " + userNum);//CONCATENATION LIKE JAVA
 
-            //CALLING METHOD IN HANDLE CLASS:
-            Console.WriteLine("currently your string says:" + handle.getWord());
-            Console.Write("enter a string: ");
-            handle.setWord(Console.ReadLine());
-            Console.Write("currently your string says: ");//WRITE -> NO CARRIAGE RETURN
-            handle.Display();
+            Console.WriteLine("Currently your string says:" + handle.getWord());//WORD FROM SimpleClass
+            Console.Write("Enter a phrase then press enter: ");
+            handle.setWord(Console.ReadLine());//STORE USER VALUE IN WORD FORM  SimpleClass
+            Console.Write("Your string now says: ");
 
-
-
+            //USING THE DEFINED CLASS METHODS TO PRINT WORD & WORD2
+            handle.Display(handle.getWord());
+            handle1.Display(handle1.getWord2());
 
         }
     }
